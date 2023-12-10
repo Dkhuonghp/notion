7c497e00b51a9ae0b5e259cbac982b5fcc1ac650
Dkhuonghp <khuonghp97@gmail.com>
2023-12-11 00:22:57 +0900

    first commit

---
+++ migrations/0000_hesitant_iron_man.sql
@@ -0,0 +1,52 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+DO $$ BEGIN
+ CREATE TYPE "key_status" AS ENUM('expired', 'invalid', 'valid', 'default');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "key_type" AS ENUM('stream_xchacha20', 'secretstream', 'secretbox', 'kdf', 'generichash', 'shorthash', 'auth', 'hmacsha256', 'hmacsha512', 'aead-det', 'aead-ietf');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_type" AS ENUM('webauthn', 'totp');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_status" AS ENUM('verified', 'unverified');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "aal_level" AS ENUM('aal3', 'aal2', 'aal1');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "code_challenge_method" AS ENUM('plain', 's256');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "workspaces" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"workspace_owner" uuid NOT NULL,
+	"title" text NOT NULL,
+	"icon_id" text NOT NULL,
+	"data" text,
+	"in_trash" text,
+	"logo" text,
+	"banner_url" text
+);
+
+*/
