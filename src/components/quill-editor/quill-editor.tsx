066bd91c3b0fa8a686664c5f23ae60870bfea70b
Dkhuonghp <khuonghp97@gmail.com>
2023-12-12 00:52:39 +0900

    update

--- src/components/quill-editor/quill-editor.tsx
+++ src/components/quill-editor/quill-editor.tsx
@@ -44,22 +44,19 @@ interface QuillEditorProps {
   dirType: 'workspace' | 'folder' | 'file';
 }
 var TOOLBAR_OPTIONS = [
+  
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+  [{ font: [] }],
   ['bold', 'italic', 'underline', 'strike'], // toggled buttons
   ['blockquote', 'code-block'],
-
-  [{ header: 1 }, { header: 2 }], // custom button values
   [{ list: 'ordered' }, { list: 'bullet' }],
   [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
   [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-  [{ direction: 'rtl' }], // text direction
-
-  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
   [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-  [{ font: [] }],
+  [{ direction: 'rtl' }], // text direction
+  ['link', 'image', 'video', 'formula'],
   [{ align: [] }],
-
   ['clean'], // remove formatting button
 ];
 
